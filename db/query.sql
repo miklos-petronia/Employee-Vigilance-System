d97c706bcbfc8470e080e289d7ad53297358cd90
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-04 03:29:25 +0100

    query to see workers by manager; select employees first name and last name then managers first name and last name; added join and where

--- db/query.sql
+++ db/query.sql
@@ -12,3 +12,10 @@ FROM employee employee
 INNER JOIN role role ON employee.role_id = role.id
 INNER JOIN department department ON role.department_id = department.id;
 
+-- query to see workers by manager
+SELECT e1.first_name AS employee_first_name, e1.last_name AS employee_last_name, 
+       e2.first_name AS manager_first_name, e2.last_name AS manager_last_name
+FROM employee e1
+JOIN employee e2 ON e1.manager_id = e2.id
+WHERE e1.manager_id IS NOT NULL;
+
