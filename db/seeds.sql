06aedff1c0bef4859d0a7932561599250c301f08
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 22:54:54 +0100

    adding employee first and last names, role_ID and managers_ID

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,3 +17,15 @@ VALUES
 ('Accountant', 125000, 3),
 ('Legal team lead', 250000, 4),
 ('Lawyer', 190000, 4);
+
+--Adding employee names; first name and last name adding also role_ID and manager_ID
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('John', 'Doe', 1, NULL), 
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, NULL),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, NULL),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, NULL),
+('Tom', 'Allen', 8, 7);
