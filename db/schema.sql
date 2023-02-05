3a7b39109fa0d7c2286d6bcb858c1278a567acee
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 22:39:41 +0100

    utilizing a database and developing a table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,13 @@
 DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
+
+-- Utilize this database
+USE employees_db;
+
+-- develop the unit table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
