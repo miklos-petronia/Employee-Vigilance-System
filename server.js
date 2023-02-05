9fd7e444bc2dc6bc95e45a9454a790eb2aa04314
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 23:19:21 +0100

    Disentangle the prompt data to receive the users options and change case to based on users feedbacks

--- server.js
+++ server.js
@@ -36,3 +36,71 @@ const startPrompt = () => {
         choices: ['View all Employees', 'View all Roles', 'View all Departments', 'View Employees by Manager', 'View Employees by Department', 'View Department Budget', 'Add Department', 'Add Employees', 'Add Role', 'Update Employee Role', 'Update Employee Managers', 'Delete Employee', 'Delete Role', 'Delete Department', 'Quit']
     }])
         .then((data) => {
+            //Disentangle the prompt data to receive the users options
+            const { doNext } = data
+            //Change case to run task based on users feedbacks
+            switch (doNext) {
+                case 'View all Employees': {
+                    viewEmployees();
+                    break
+                }
+                case 'View all Roles': {
+                    viewRoles();
+                    break
+                }
+                case 'View all Departments': {
+                    viewDepartment();
+                    break
+                }
+                case 'View Employees by Manager': {
+                    viewEmployeesByManager();
+                    break
+                }
+                case 'View Employees by Department': {
+                    viewEmployeesByDepartment();
+                    break
+                }
+                case 'View Department Budget': {
+                    viewDepartmentBudget();
+                    break
+                }
+                case 'Add Employees': {
+                    addEmployees();
+                    break
+                }
+                case 'Add Role': {
+                    addRole();
+                    break
+                }
+                case 'Add Department': {
+                    addDepartment();
+                    break
+                }
+                case 'Update Employee Role': {
+                    updateEmployeeRole();
+                    break
+                }
+                case 'Update Employee Managers': {
+                    updateEmployeeManager();
+                    break
+                }
+                case 'Delete Employee': {
+                    deleteEmployee();
+                    break
+                }
+                case 'Delete Role': {
+                    deleteRole();
+                    break
+                }
+                case 'Delete Department': {
+                    deleteDepartment();
+                    break
+                }
+                case 'Quit': {
+                    connection.end()
+                    break
+                }
+
+            }
+        })
+}
