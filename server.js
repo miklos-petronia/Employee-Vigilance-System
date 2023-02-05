f78965ec7f6bd7ca7e1ad3fb437855a8da5de374
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 23:03:59 +0100

    importing mysql2 and elements of inquirer

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+// Essential inquirer
+const inquirer = require('inquirer');
+//Essential Console.Table Cleaner table in console log detach index.
+const cTable = require('console.table');
+
