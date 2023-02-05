4afe9c607543fa374af505aeec0a5eac78d77ca2
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 22:42:16 +0100

    developing the employee table

--- db/schema.sql
+++ db/schema.sql
@@ -21,3 +21,15 @@ CREATE TABLE role (
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+-- Developing the employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) 
+
+);
