09268fc6908c58ec50833a0619e398e4198f7e3e
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 23:09:12 +0100

    adding express middleware

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
