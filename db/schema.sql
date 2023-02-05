ba2755958b7e082c0886f450925ae9efb973f903
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 22:41:02 +0100

    Developing a role table

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE department (
   name VARCHAR(30)
  
 );
+
+-- Developing the role table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
