542e0045e7af291fb15f4b97d5c87d24f20f63fa
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 22:48:25 +0100

    adding within the roles the title, salary and department_ID

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,14 @@ VALUES
 ('Finance'),
 ('Legal');
 
+--Adding within the roles the title, salary and the department ID
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2), 
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3), 
+('Accountant', 125000, 3),
+('Legal team lead', 250000, 4),
+('Lawyer', 190000, 4);
