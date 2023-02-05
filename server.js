d1c13619a24c4a012624bc97374145bc6a9cec75
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-05 23:12:04 +0100

    added attached to database and mysql username, password and a console.log

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Attach to database
+const connection = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password 
+        password: '',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`),
+);
